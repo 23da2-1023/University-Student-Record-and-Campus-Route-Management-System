ecb3e18a26cf34254ccfbb3a46a9cb6b6b173b63
23da2-1054 <https://github.com/23da2-1054>
2026-09-26 13:16:49 +0700

    Implement insert with chaining in StudentHashTable

--- src/StudentHashTable.java
+++ src/StudentHashTable.java
@@ -11,12 +11,23 @@ public class StudentHashTable {
     @SuppressWarnings("unchecked")
     public StudentHashTable() { buckets = new Node[CAPACITY]; }
 
-    
     private int hash(String studentId) {
         int hash = 0;
         for (char c : studentId.toUpperCase().toCharArray()) {
             hash = (hash * 31 + c) % CAPACITY;
         }
         return Math.abs(hash);
+    }
+
+public void insert(Student student) {
+        int index = hash(student.getStudentId());
+        Node newNode = new Node(student);
+        if (buckets[index] == null) {
+            buckets[index] = newNode;
+        } else {
+            Node current = buckets[index];
+            while (current.next != null) current = current.next;
+            current.next = newNode;
+        }
     }
 }
